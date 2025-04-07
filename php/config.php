9f80fac8cf839b6d6d3da9c5e21b5d03454703e0
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-04-07 11:02:58 -0400

    Modified logic so that the timer only starts when "Start Challenge" is clicked rather than when index.php is loaded

--- php/config.php
+++ php/config.php
@@ -6,8 +6,13 @@ define("MAX_HINTS", 3);
 define("TOTAL_PUZZLES", 3);
 
 // Reset game state if starting fresh
-if (!isset($_SESSION['game_started'])) {
+// This ensures the timer ONLY starts when "Start Challenge" is clicked
+if (isset($_POST['start_game'])) {
     $_SESSION['game_started'] = time();
+    $_SESSION['hints_used'] = 0;
+    $_SESSION['completed_puzzles'] = [];
+    $_SESSION['current_puzzle'] = 1;
+} elseif (!isset($_SESSION['hints_used'])) { // Otherwise just ensure the basic session vars exist
     $_SESSION['hints_used'] = 0;
     $_SESSION['completed_puzzles'] = [];
     $_SESSION['current_puzzle'] = 1;
